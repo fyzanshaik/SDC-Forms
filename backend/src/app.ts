a4dc1a6054c1bfeb57df1409675441c37179ba64
fyzanshaik <fyzan.shaik@gmail.com>
2024-10-22 16:34:50 +0530

    port

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import userRouter from './route';
 
 const app = express();
-
+const PORT = process.env.PORT || 8080;
 app.use(
 	cors({
 		origin: ['http://localhost:5173', 'https://sdc-forms.vercel.app', 'https://sdc-forms-git-main-fyzanshaiks-projects.vercel.app', 'https://sdc-forms-86wal909i-fyzanshaiks-projects.vercel.app'],
@@ -14,6 +14,6 @@ app.use(express.json());
 
 app.use('/api', userRouter);
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
 	console.log(`Server is running on PORT 8080`);
 });
